c33171e8ab43c851ce49384e3e161406df88f91a
eVgenos <evgenos2709@gmail.com>
2017-03-12 02:48:18 +0200

    Commit for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.all.add_role :admin
+User.first.add_role :admin
