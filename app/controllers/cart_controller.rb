897caa958c19d223f6faeca4684321c6ba9c42ea
eVgenos <evgenos2709@gmail.com>
2017-03-14 01:39:57 +0200

    Add cart

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,22 @@
+class CartController < ApplicationController
+  def index
+    @cart = session[:cart] || {}
+  end
+
+  def add
+    id = params[:id]
+    cart = session[:cart] ||= {}
+    cart[id] = (cart[id] || 0) + 1
+
+    redirect_to products_path
+  end
+
+  def destroy
+    id = params[:id]
+    cart = session[:cart]
+
+    cart.delete id
+
+    redirect_to cart_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'cart' => 'cart#index', as: 'cart'
+  delete 'cart/:id' => 'cart#destroy', as: 'cart_des'
+  get 'cart/add/:id' => 'cart#add', as: 'cart_add'
+
   get 'admin' => 'admin#index'
   put 'admin/:id' => 'admin#update'
   patch 'admin/:id' => 'admin#update'
-  get 'admin/edit_products', to: 'admin#edit_products', as: 'edit_products'
-  get 'admin/edit_categories', to: 'admin#edit_categories', as: 'edit_categories'
-  get 'admin/edit_users', to: 'admin#edit_users', as: 'edit_users'
+  get 'admin/edit_products' => 'admin#edit_products', as: 'edit_products'
+  get 'admin/edit_categories' => 'admin#edit_categories', as: 'edit_categories'
+  get 'admin/edit_users' => 'admin#edit_users', as: 'edit_users'
 
   devise_for :users
 
